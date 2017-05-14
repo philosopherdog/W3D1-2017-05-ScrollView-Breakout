05988dfd24baec01bd80e574ebde3a055031e31c
steve thompson <steven.thompson.ca@gmail.com>
2017-05-14 13:12:35 -0400

    Initial Commit

---
+++ W3D1-ScrollPager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W3D1-ScrollPager
+//
+//  Created by steve on 2017-05-14.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ W3D1-ScrollPager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  W3D1-ScrollPager
+//
+//  Created by steve on 2017-05-14.
+//  Copyright © 2017 steve. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+
+@end
